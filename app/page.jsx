6351db50fd517d9c2afc9166eb6dd87c760c5672
Matt <matt@192.168.1.13>
2025-08-26 03:18:18 +0300

    fixed image paths

--- app/page.jsx
+++ app/page.jsx
@@ -6,16 +6,16 @@ export default function Home() {
     <section className="flex flex-col gap-5 px-3 md:px-6 max-w-[1920px] mx-auto">
       <SplitText text="Projects" className="text-3xl md:text-6xl font-bold" />
       <div id="profiles" className="flex flex-row flex-wrap gap-5 w-fill h-fit">
-        <Project href="/parametr" src="/parametrCover.png" title="Parametr website" subtitle="UI/UX, Communication" />
-        <Project href="/adc" src="/adcCover.png" title="ADC community" subtitle="Branding, UI/UX, Dev, Motion" />
-        <Project href="/postcraft" src="/postcraftCover.png" title="Postcraft — figma HTML generator" subtitle="UI/UX, Dev, Management" />
-        <Project href="/babank" src="/babankCover.png" title="BaBank" subtitle="UI/UX, Digital" />
-        <Project href="/pik_hr" src="/pikHrCover.png" title="PIK x HH.RU — HR multi-platform" subtitle="UI, Web, Management" />
-        <Project href="/adc_hub" src="/adcHubCover.png" title="ADC Hub — social network" subtitle="Branding, UI/UX, Dev, Motion" />
-        <Project href="" src="/samoletCover.png" title="Samolet — COMING SOON" subtitle="UI/UX, Digital, Dev" />
-        <Project href="" src="/marketCover.png" title="Yandex Market — COMING SOON" subtitle="UI/UX" />
-        <Project href="/mailru" src="/mailCover.png" title="Mail.ru — rebranding concept" subtitle="Branding" />
-        <Project href="/dada" src="/dadaCover.png" title="Neo-Dada Manifesto" subtitle="Motion, Creative coding" />
+        <Project href="/parametr" src="/ParametrCover.png" title="Parametr website" subtitle="UI/UX, Communication" />
+        <Project href="/adc" src="/ADCCover.png" title="ADC community" subtitle="Branding, UI/UX, Dev, Motion" />
+        <Project href="/postcraft" src="/PostcraftCover.png" title="Postcraft — figma HTML generator" subtitle="UI/UX, Dev, Management" />
+        <Project href="/babank" src="/BaBankCover.png" title="BaBank" subtitle="UI/UX, Digital" />
+        <Project href="/pik_hr" src="/PikHrCover.png" title="PIK x HH.RU — HR multi-platform" subtitle="UI, Web, Management" />
+        <Project href="/adc_hub" src="/AdcHubCover.png" title="ADC Hub — social network" subtitle="Branding, UI/UX, Dev, Motion" />
+        <Project href="" src="/SamoletCover.png" title="Samolet — COMING SOON" subtitle="UI/UX, Digital, Dev" />
+        <Project href="" src="/MarketCover.png" title="Yandex Market — COMING SOON" subtitle="UI/UX" />
+        <Project href="/mailru" src="/MailCover.png" title="Mail.ru — rebranding concept" subtitle="Branding" />
+        <Project href="/dada" src="/DadaCover.png" title="Neo-Dada Manifesto" subtitle="Motion, Creative coding" />
       </div>
     </section>
   );
